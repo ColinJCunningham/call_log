aa1b6bde2ba04ecc6b3c3927590aaa567f9f099b
ColinJCunningham <cunninghamwebdesign@gmail.com>
2022-03-24 16:50:29 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Box, Grommet } from "grommet";
+import Nav from "./Components/Header/index";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Grommet background='light-5' full={true}>
+			<Nav />
+			<Box
+				direction='row'
+				border={{ color: "dark-1", size: "small" }}
+				pad='xsmall'>
+				<Box pad='small' background='dark-3' />
+				<Box pad='medium' background='light-3' />
+			</Box>
+		</Grommet>
+	);
 }
 
 export default App;
---
+++ src/Components/Header/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Box, Grommet, Header, Button, Clock, Image, Heading } from "grommet";
+import { Vulnerability, Notes, Phone, Analytics } from "grommet-icons";
+import Logo from "./Logo.png";
+
+const myTheme = {
+	heading: {
+		font: {
+			family: "Kanit",
+			size: "xxlarge",
+		},
+	},
+};
+
+export default function Nav() {
+	return (
+		<Grommet theme={myTheme} pad='none'>
+			<Header background='dark-1' pad='medium'>
+				<Button background='dark-1' label='Refresh' hoverIndicator />
+				<Box direction='row' pad='none' elevation='xxsmall'>
+					{/* Left Section ^^^^^^   */}
+					<Box pad='none' direction='row'>
+						<Heading
+							style={{ fontSize: "3.4rem" }}
+							margin={{ horizontal: "large", vertical: "none" }}
+							color='light-2'
+							level='2'>
+							Call Logger
+							<span
+								style={{
+									paddingTop: "1rem",
+									fontSize: "1.4rem",
+									color: "#FF4040",
+								}}>
+								V.1
+							</span>
+						</Heading>
+						<Box pad={{ top: "small" }} direction='row'>
+							<div
+								style={{
+									margin: "2% 5% 1% 1%",
+								}}>
+								<Notes size='medium' color='light-1' />
+							</div>
+							<div
+								style={{
+									margin: "2% 5% 2% 1%",
+								}}>
+								<Phone size='medium' color='status-ok' />
+							</div>
+							<div
+								style={{
+									margin: "2% 5% 2% 1%",
+								}}>
+								<Analytics size='medium' color='status-critical' />
+							</div>
+						</Box>
+					</Box>
+				</Box>
+
+				<Clock type='digital' />
+			</Header>
+		</Grommet>
+	);
+}
